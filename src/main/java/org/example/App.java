3f18155d1c85a155eba3eede223ab0070f015ac4
Son Nghiem <nghiemminhson@gmail.com>
2022-10-14 20:52:33 +0200

    meet Approval demands

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,49 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        boolean runApp = true;
+
+        while (runApp) {
+            System.out.println("Enter first number: ");
+            double num1 = scanner.nextDouble();
+            System.out.println("Enter second number: ");
+            double num2 = scanner.nextDouble();
+            System.out.println("Enter the arithmetic operator '+', '-', '*', '/': ");
+            char operator = scanner.next().charAt(0);
+            double result = 0;
+
+
+            //selections
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default: {
+                    System.out.println("Character " + operator + " is not correct!");
+                    return;
+                }
+            }
+
+            //Output
+            System.out.println("The result is: " + result);
+            System.out.println("Do you want to continue? (y/n)");
+            char userChoice = scanner.next().charAt(0);
+            if (userChoice == 'n') {
+                System.out.println("Program ended. Thank you for using our application!");
+                runApp = false;
+            } else if (userChoice == 'y'){
+                runApp = true;
+            }
+        }
     }
 }
