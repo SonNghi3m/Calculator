7ac7b1775e33e5f494c02d3a81b27391562108fd
Son Nghiem <nghiemminhson@gmail.com>
2022-10-15 14:11:21 +0200

    version 1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,56 +2,47 @@ package org.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
         boolean runApp = true;
-
         while (runApp) {
-            System.out.println("Enter first number: ");
-            double num1 = scanner.nextDouble();
-            System.out.println("Enter second number: ");
-            double num2 = scanner.nextDouble();
-            System.out.println("Enter the arithmetic operator '+', '-', '*', '/': ");
-            char operator = scanner.next().charAt(0);
-            double result = 0;
-
-
-            //selections
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default: {
-                    System.out.println("Character " + operator + " is not correct!");
-                    return;
-                }
-            }
-
-            //Output
-            System.out.println("The result is: " + result);
-            System.out.println("Do you want to continue? (y/n)");
-            char userChoice = scanner.next().charAt(0);
-            if (userChoice == 'n') {
-                System.out.println("Program ended. Thank you for using our application!");
-                runApp = false;
-            } else if (userChoice == 'y'){
-                runApp = true;
+            System.out.println("Welcome to console-based calculator! You can select one of the following options: ");
+            System.out.println(" 1 - Addition");
+            System.out.println(" 2 - Subtraction");
+            System.out.println(" 3 - multiplication");
+            System.out.println(" 4 - division");
+            System.out.println(" 5 - exit");
+            int option = scanner.nextInt();
+            if (option == 1) {
+                System.out.println("Enter first number: ");
+                double num1 = scanner.nextDouble();
+                System.out.println("Enter second number: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("The result is: " + (num1 + num2));
+            } else if (option == 2) {
+                System.out.println("Enter first number: ");
+                double num1 = scanner.nextDouble();
+                System.out.println("Enter second number: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("The result is: " + (num1 - num2));
+            } else if (option == 3) {
+                System.out.println("Enter first number: ");
+                double num1 = scanner.nextDouble();
+                System.out.println("Enter second number: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("The result is: " + (num1 * num2));
+            } else if (option == 4) {
+                System.out.println("Enter first number: ");
+                double num1 = scanner.nextDouble();
+                System.out.println("Enter second number: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("The result is: " + (num1 / num2));
+            } else if (option == 5) {
+                System.out.println("Thank you for using our app!");
+                break;
             }
         }
     }
